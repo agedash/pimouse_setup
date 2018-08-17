ae14b07c591dd4a647c751769511cc7b5e435578
Shinichi Fujiwara <anosorawokoeteyuku@yahoo.co.jp>
2018-08-17 15:06:09 +0000

    Change the setup way with cron is not standard, Make file is prepared. Moreover, the setup script is placed to /etc/init.d.

--- setup.bash
+++ setup.bash
@@ -10,10 +10,14 @@ chmod 666 /dev/rt*
 
 echo 0 > /dev/rtmotoren0
 echo 1 | tee /dev/rtled*
-sleep 3
+sleep 2
 echo 0 | tee /dev/rtled*
 
 echo 300 > /dev/rtbuzzer0
-sleep 3
+sleep 1
+echo 500 > /dev/rtbuzzer0
+sleep 1
+echo 700 > /dev/rtbuzzer0
+sleep 1
 echo 0 > /dev/rtbuzzer0
 
